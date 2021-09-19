06e7cf0fe2f0cd5b7e9a0d0d8b6cb714afb86ee0
Richard Walker <Dopameme@outlook.com>
2021-09-19 16:49:18 -0400

    Used Shoe variable in fragment_shoe_detail.xml for the editText bindings

--- app/src/main/java/com/udacity/shoestore/ShoeListViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeListViewModel.kt
@@ -1,26 +1,15 @@
 package com.udacity.shoestore
-
-import androidx.databinding.BaseObservable
 import androidx.lifecycle.*
 import com.udacity.shoestore.models.Shoe
 
 class ShoeListViewModel : ViewModel() {
     private var _shoes = MutableLiveData<MutableList<Shoe>>() // List of Shoe Class
 
-    var shoeName = MutableLiveData<String>()
-    var shoeCompany = MutableLiveData<String>()
-    var shoeSize = MutableLiveData<String>()
-    var shoeDesc = MutableLiveData<String>()
-
     val shoe : MutableLiveData<MutableList<Shoe>> get() = _shoes // return List of Shoe Class
 
-    fun addShoe() {
-        val addingShoe = Shoe(shoeName.toString(), shoeSize.toString(), shoeCompany.toString(),
-            shoeDesc.toString())
+    fun addShoe(shoe: Shoe) {
+        val newShoe = Shoe(shoe.name, shoe.size, shoe.company, shoe.description)
 
-        _shoes.value?.add(addingShoe)
+        _shoes.value?.add(newShoe)
     }
-
-
-
 }
